23e2715729b5194e9ba7a52220b305bc710e94d1
andypymont <andypymont@gmail.com>
2023-01-15 17:00:27 +0000

    Day 21 Part 2

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
@@ -16,6 +16,15 @@ impl Operation {
             Self::Subtract => a - b,
             Self::Multiply => a * b,
             Self::Divide => a / b,
+        }
+    }
+
+    fn inputs_to_get_value(&self, current_a: i64, current_b: i64, target: i64) -> (i64, i64) {
+        match self {
+            Self::Add => (target - current_b, target - current_a),
+            Self::Subtract => (target + current_b, current_a - target),
+            Self::Divide => (target * current_b, current_a / target),
+            Self::Multiply => (target / current_b, target / current_a),
         }
     }
 }
@@ -53,6 +62,21 @@ impl Monkey {
             }
         }
     }
+
+    fn inputs_to_get_value(
+        &self,
+        monkeys: &HashMap<String, Monkey>,
+        target: i64,
+    ) -> Option<(i64, i64)> {
+        match self {
+            Monkey::Value(_) => None,
+            Monkey::Calculation(a, op, b) => {
+                let a = monkeys.get(a).unwrap_or(&Monkey::Value(0));
+                let b = monkeys.get(b).unwrap_or(&Monkey::Value(0));
+                Some(op.inputs_to_get_value(a.value(monkeys), b.value(monkeys), target))
+            }
+        }
+    }
 }
 
 fn parse_monkeys(input: &str) -> HashMap<String, Monkey> {
@@ -83,8 +107,35 @@ pub fn part_one(input: &str) -> Option<i64> {
     monkeys.get("root").map(|monkey| monkey.value(&monkeys))
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let mut monkeys = parse_monkeys(input);
+
+    if let Some(Monkey::Calculation(a, _, b)) = monkeys.get("root") {
+        let mut queue = VecDeque::new();
+        monkeys.insert(
+            "root".to_string(),
+            Monkey::Calculation(a.to_string(), Operation::Subtract, b.to_string()),
+        );
+        queue.push_front(("root", 0));
+
+        while let Some((name, expected)) = queue.pop_front() {
+            if name == "humn" {
+                return Some(expected);
+            }
+
+            let monkey = monkeys.get(name).unwrap_or(&Monkey::Value(0));
+            if let Monkey::Calculation(a, _, b) = monkey {
+                if let Some((expect_a, expect_b)) = monkey.inputs_to_get_value(&monkeys, expected) {
+                    queue.push_back((a, expect_a));
+                    queue.push_back((b, expect_b));
+                }
+            }
+        }
+
+        None
+    } else {
+        None
+    }
 }
 
 fn main() {
@@ -139,6 +190,26 @@ mod tests {
     }
 
     #[test]
+    fn test_monkey_inputs_to_get_value() {
+        let mut monkeys = HashMap::new();
+
+        monkeys.insert("pppw".to_string(), Monkey::Value(9));
+        monkeys.insert("sjmn".to_string(), Monkey::Value(3));
+
+        let add = Monkey::Calculation("pppw".to_string(), Operation::Add, "sjmn".to_string());
+        let sub = Monkey::Calculation("pppw".to_string(), Operation::Subtract, "sjmn".to_string());
+        let div = Monkey::Calculation("pppw".to_string(), Operation::Divide, "sjmn".to_string());
+        let mul = Monkey::Calculation("pppw".to_string(), Operation::Multiply, "sjmn".to_string());
+        let val = Monkey::Value(4);
+
+        assert_eq!(add.inputs_to_get_value(&monkeys, 6), Some((3, -3)));
+        assert_eq!(sub.inputs_to_get_value(&monkeys, 10), Some((13, -1)));
+        assert_eq!(div.inputs_to_get_value(&monkeys, 9), Some((27, 1)));
+        assert_eq!(mul.inputs_to_get_value(&monkeys, 54), Some((18, 6)));
+        assert_eq!(val.inputs_to_get_value(&monkeys, 18), None);
+    }
+
+    #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 21);
         assert_eq!(part_one(&input), Some(152));
@@ -147,6 +218,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 21);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(301));
     }
 }
